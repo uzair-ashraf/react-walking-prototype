37bb81b336b440719c3f911f94f50f7c92e8eca5
Uzair Ashraf <uzinatorcl@live.com>
2020-09-06 14:11:19 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const srcPath = path.resolve(__dirname, 'src');
+const publicPath = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  entry: './src',
+  output: {
+    path: publicPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: srcPath,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              '@babel/plugin-transform-react-jsx'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    contentBase: publicPath,
+    historyApiFallback: true,
+    watchContentBase: true,
+    stats: 'minimal'
+  }
+};
